cf42de9f84ca824331b5fa7f61131b5fac188717
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-05-07 23:59:21 +0530

    deleted

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,43 +1,26 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './Component/Navbar';
-import Home from './Page/Home';
-import Search from './Page/Search';
-import Login from './Page/Login';
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Page/Home'
+import Navbar from './Component/Navbar'
+import Search from './Page/Search'
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true);
-  };
-
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
 
   return (
-    <BrowserRouter>
-      {isLoggedIn && <Navbar onLogout={handleLogout} />}
-      <Routes>
-        <Route
-          path="/"
-          element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/search"
-          element={isLoggedIn ? <Search /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/login"
-          element={<Login onLoginSuccess={handleLoginSuccess} />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
 }
 
-export default App;
+export default App
